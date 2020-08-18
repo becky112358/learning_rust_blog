b3ece33679750ac628abce691e3757b14e2ed69f
becky112358 <8b2@protonmail.com>
2020-08-18 16:12:47 +0300

    Code from book: Add the states and behaviours as types method.

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,28 @@
 mod method_state_pattern;
+mod method_states_and_behaviours_as_types;
 
 fn main() {
-    let mut post = method_state_pattern::Post::new();
+    let mut post0 = method_state_pattern::Post::new();
 
-    post.add_text("Go green!");
-    assert_eq!("", post.content());
+    post0.add_text("Go green!");
+    assert_eq!("", post0.content());
 
-    post.request_review();
-    assert_eq!("", post.content());
+    post0.request_review();
+    assert_eq!("", post0.content());
 
-    post.approve();
-    assert_eq!("Go green!", post.content());
-    println!("{}", post.content());
+    post0.approve();
+    assert_eq!("Go green!", post0.content());
+    println!("{}", post0.content());
+
+
+    let mut post1 = method_states_and_behaviours_as_types::Post::new();
+
+    post1.add_text("Green green green!!");
+
+    let post1 = post1.request_review();
+
+    let post1 = post1.approve();
+
+    assert_eq!("Green green green!!", post1.content());
+    println!("{}", post1.content());
 }
