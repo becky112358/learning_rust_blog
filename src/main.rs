97d18616f8be24dabf8b826864a07357ae94cb0d
becky112358 <8b2@protonmail.com>
2020-08-18 16:07:52 +0300

    Code from book: Add the state pattern method.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+mod method_state_pattern;
+
 fn main() {
-    println!("Hello, world!");
+    let mut post = method_state_pattern::Post::new();
+
+    post.add_text("Go green!");
+    assert_eq!("", post.content());
+
+    post.request_review();
+    assert_eq!("", post.content());
+
+    post.approve();
+    assert_eq!("Go green!", post.content());
+    println!("{}", post.content());
 }
